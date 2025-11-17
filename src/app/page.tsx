44c2fd8a2f64c0cc2430450ce245f518d48e7933
arshad <arshadpyaseen@gmail.com>
2025-11-17 17:19:01 +0530

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Markdown from "react-markdown";
+
+const markdown = `
+# Hello, world!
+
+This is a test of the markdown component.
+`;
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans"></div>
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <article className="verse-zinc [--verse-spacing:17px]">
+        <Markdown>{markdown}</Markdown>
+      </article>
+    </div>
   );
 }
